088cec768fa7530a8fa8240639ab965ea052766f
Ezra <ezrae18@gmail.com>
2024-09-09 13:51:25 +0200

    Configure Sphinx for Django

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,10 @@
 
 import os
 import sys
+import django
 sys.path.insert(0, os.path.abspath(".."))
+os.environ["DJANGO_SETTINGS_MODULE"] = "core.settings"
+django.setup()
 
 project = 'Amee&SobosSite'
 copyright = '2024, Ezra Moosa'
