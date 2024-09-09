4089152e9d3fbbdcc3e06a6491a7cc1522d97e22
Ezra <ezrae18@gmail.com>
2024-09-09 13:47:00 +0200

    Imported os and sys. Updated theme

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))
+
 project = 'Amee&SobosSite'
 copyright = '2024, Ezra Moosa'
 author = 'Ezra Moosa'
@@ -28,5 +32,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx-rtd-theme'
 html_static_path = ['_static']
