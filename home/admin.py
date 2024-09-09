e652450858b258f4e85e72cf9243da901956d5a7
Ezra <ezrae18@gmail.com>
2024-09-09 13:29:53 +0200

    Update docstrings for home/admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,19 @@
+"""
+This module registers the models for the Django admin interface.
 
-#Libraries and modules
+Models:
+-------
+- Song: Represents a song by the band.
+- Member: Represents a band member and their details.
+- Event: Represents an event where the band performs.
+
+The registered models will be accessible and manageable via the Django admin
+panel.
+"""
+
+# Libraries and modules
 from django.contrib import admin
 from .models import Song, Member, Event
-
 
 # Register models for use in admin panel
 admin.site.register(Song)
