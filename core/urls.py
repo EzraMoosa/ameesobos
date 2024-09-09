d28fafaba706e8186c2a08800131cda9f9fd03b2
Ezra <ezrae18@gmail.com>
2024-09-09 05:04:06 +0200

    Updated docstrings for core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,19 @@
 from django.contrib import admin
 from django.urls import path, include
 
+"""
+This module defines the URL patterns for the Django project.
+
+It maps the root URL and additional paths to their respective URLconfs.
+
+URL Patterns:
+-------------
+- ``'admin/'``: Maps to the Django admin site.
+- ``''``: Includes URL patterns from the `home` app.
+- ``'user_auth/'``: Includes URL patterns from the `user_auth` app.
+
+This file serves as the central routing configuration for the project.
+"""
 
 urlpatterns = [
     path('admin/', admin.site.urls),
