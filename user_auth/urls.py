08c89509085770c46860b0704cb2edd899609990
Ezra <ezrae18@gmail.com>
2024-09-09 05:06:47 +0200

    Updated docstrings for user_auth/urls.py

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -2,6 +2,19 @@
 from django.urls import path
 from . import views
 
+"""
+URL configuration for the `user_auth` app.
+
+This module defines the URL patterns for handling user authentication-related
+views in the `user_auth` app.
+
+URL Patterns:
+-------------
+- ``''``: Maps to the `user_login` view, which handles user login.
+- ``'authenticate_user'``: Maps to the `authenticate_user` view.
+- ``'logout/'``: Maps to the `user_logout` view, which handles user logout.
+- ``'register/'``: Maps to the `register` view, for user registration.
+"""
 
 app_name = 'user_auth'
 
