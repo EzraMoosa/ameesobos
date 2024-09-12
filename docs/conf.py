6d386be20298b93d2d47fa316f4f84326efba22d
Ezra <ezrae18@gmail.com>
2024-09-12 13:51:20 +0200

    Corrected MODULES - MODULE

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ import os
 import sys
 import django
 sys.path.insert(0, os.path.abspath(".."))
-os.environ['DJANGO_SETTINGS_MODULES'] = 'core.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
 django.setup()
 
 
